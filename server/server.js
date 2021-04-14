32ba359c71c5d748c4e0ec05bc072e43f4761d2d
Pete <peterwbrodie@gmail.com>
2021-04-14 11:53:40 +0100

    service complete

--- server/server.js
+++ server/server.js
@@ -5,3 +5,19 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('hotel');
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
